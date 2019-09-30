2aa767a9e375b749fa9cb590a791cdc33d5926cf
Krishnappa <SKrishnappa@ad.harman.com>
2019-09-30 18:01:34 +0530

    cors is added

--- info-tracker/src/main/java/com/harman/infotracker/controller/TrackerController.java
+++ info-tracker/src/main/java/com/harman/infotracker/controller/TrackerController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.harman.infotracker.model.PoiDetail;
 import com.harman.infotracker.respository.PoiRepository;
 import com.harman.infotracker.respository.TrackerRepository;
 
+@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:8080" })
 @RestController
 @RequestMapping("/api")
 public class TrackerController {
