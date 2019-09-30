e7e7cca7616a97d332e62d913dacac042a6c78bc
Som0306 <41955032+Som0306@users.noreply.github.com>
2019-09-30 11:21:05 +0530

    First Commit - Project Initialization

---
+++ info-tracker/src/test/java/com/harman/infotracker/InfoTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.harman.infotracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfoTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
